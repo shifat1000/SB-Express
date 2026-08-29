0962b5f4abc7ef0ad43866cf91b5c8bd0865393f
5h1f47 <130339785+shifat1000@users.noreply.github.com>
2026-08-29 17:17:43 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='agent-point-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./header.jpg'])));self.skipWaiting()});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
